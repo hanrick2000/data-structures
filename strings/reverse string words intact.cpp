ba23723ba2d99caa7fbf42ed203b7ccf01b40088
Tanvi Surana <tanvisurana20@gmail.com>
2017-01-09 12:52:01 +0530

    Update reverse string words intact.cpp

--- strings/reverse string words intact.cpp
+++ strings/reverse string words intact.cpp
@@ -39,7 +39,36 @@ void reverseString(char *a)
 
     reverse(a,temp-1);  
 }
-	
+
+void reverseWords(string &s) {
+            string ans = "";
+            string cur = "";
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s[i] == ' ') {
+                    if (cur.length() == 0) {
+                        continue;
+                    }
+                    // found a word 
+                    reverse(cur.begin(), cur.end());
+                    if (ans.length() > 0) { 
+                        ans.push_back(' ');
+                    }
+                    ans += cur;
+                    cur = "";
+                    continue;
+                }
+                cur.push_back(s[i]);
+            }
+            if (cur.length() > 0) {
+                reverse(cur.begin(), cur.end());
+                if (ans.length() > 0) { 
+                    ans.push_back(' ');
+                }
+                ans += cur;
+            }
+            s = ans;
+            return;
+        }
 int main()
 {
     char a[100]="Welcome to this world"; 
